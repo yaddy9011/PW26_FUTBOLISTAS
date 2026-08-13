d6d1edae8b54690a255fc231efdf6b0a591235ab
AriSuperPro <arielhernandezhernandez@gmail.com>
2026-08-13 08:48:31 -0600

    diseño

--- frontend/src/components/Tarjeta.jsx
+++ frontend/src/components/Tarjeta.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 function Tarjeta({ item, onEliminar }) {
     return (<article className="tarjeta">
-        <img src={item.imagen} alt={item.nombre} />
+        <img src={item.imagen} alt={item.nombre} 
+        className="foto-futbolista"/>
         <h2>{item.nombre}</h2> <p><strong>Equipo:</strong> {item.equipo}</p>
         <p><strong>Posición:</strong> {item.posicion}</p>
         <p><strong>Nacionalidad:</strong> {item.nacionalidad}</p>
